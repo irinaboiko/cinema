6aff930dc192e02eb74d0eb968f06165440f820c
Irina Boiko <irinaboiko95@gmail.com>
2021-11-23 16:48:16 +0300

    fix client routes

--- client/src/router/Routes.tsx
+++ client/src/router/Routes.tsx
@@ -1,15 +1,23 @@
 import React, { FC } from "react";
 import { Route, Routes } from "react-router-dom";
 
-import HomePageContainer from "../pages/HomePage/containers/HomePageContainer";
 import { ROUTES } from "./routeNames";
+import HomePageContainer from "../pages/HomePage/containers/HomePageContainer";
+import LoginPageContainer from "../pages/LoginPage/containers/LoginPageContainer";
+import UserPageContainer from "../pages/UserPage/containers/UserPageContainer";
+import MoviesPageContainer from "../pages/MoviesPage/containers/MoviesPageContainer";
+import AdminPanelPageContainer from "../pages/AdminPanelPage/containers/AdminPanelPageContainer";
 
-const Routes: FC = () => {
+const RoutesComponent: FC = () => {
   return (
     <Routes>
-      <Route path={ROUTES.HOME} element={HomePageContainer} />
+      <Route path={ROUTES.LOGIN} element={<LoginPageContainer />} />
+      <Route path={ROUTES.HOME} element={<HomePageContainer />} />
+      <Route path={ROUTES.USER} element={<UserPageContainer />} />
+      <Route path={ROUTES.MOVIES} element={<MoviesPageContainer />} />
+      <Route path={ROUTES.ADMIN} element={<AdminPanelPageContainer />} />
     </Routes>
   );
 };
 
-export default Routes;
+export default RoutesComponent;
